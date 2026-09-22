bf403052b010364b3271622a72b8c6199ee77423
snchzzero <aaevstafev@gmail.com>
2026-09-22 13:40:53 +0300

    add: 3.2.5-3.2.7

---
+++ Professional /module_3/ex_3.2.7.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+florida_hurricane_dates = [
+    date(1987, 11, 15),
+    date(1988, 3, 12),
+    date(1976, 5, 2)
+]
+
+# присваиваем самую раннюю дату урагана в переменную first_date
+first_date = min(florida_hurricane_dates)
+
+# конвертируем дату в ISO и RU формат
+iso = 'Дата первого урагана в ISO формате: ' + first_date.isoformat()
+ru = 'Дата первого урагана в RU формате: ' + first_date.strftime('%d.%m.%Y')
+us = 'Дата первого урагана в US формате: ' + first_date.strftime('%m/%d/%Y')
+
+print(iso)
+print(ru)
+print(us)
